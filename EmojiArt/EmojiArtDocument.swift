75d859dc7c53b72e3f2f15440ab0a1c8c2b4c615
Kira <jooppp@live.com>
2021-02-17 20:50:23 +0900

    Complete  url image drop feature ( background image can be set)

--- EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArtDocument.swift
@@ -12,5 +12,47 @@ class EmojiArtDocument: ObservableObject {
     
     static let palette: String = "🍏🍎🍐🍊🍋🍌"
     
+    @Published private var emojiArt: EmojiArt = EmojiArt()
+    
+    @Published private(set) var backgroundImage : UIImage?
+    
+    // MARK: - Intent(s)
+    
+    func addEmoji(_ emoji: String, at location: CGPoint, size: CGFloat) {
+        emojiArt.addEmoji(emoji, x: Int(location.x), y: Int(location.y), size: Int(size))
+    }
+    
+    func moveEmoji(_ emoji: EmojiArt.Emoji, by offset: CGSize) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].x += Int(offset.width)
+            emojiArt.emojis[index].y += Int(offset.height)
+        }
+    }
+    
+    func scaleEmoji(_ emoji: EmojiArt.Emoji, by scale: CGFloat) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].size = Int((CGFloat(emojiArt.emojis[index].size) * scale).rounded(.toNearestOrEven))
+        }
+    }
+    
+    func setBackgroundURL (_ url: URL?) {
+        emojiArt.backgroundURL = url?.imageURL
+        fetchBackgroundImageData()
+    }
+    
+    private func fetchBackgroundImageData() {
+        backgroundImage = nil
+        if let url = self.emojiArt.backgroundURL {
+            DispatchQueue.global(qos: .userInitiated).async {
+                if let imageData = try? Data(contentsOf: url) {
+                    DispatchQueue.main.async {
+                        if url == self.emojiArt.backgroundURL{
+                            self.backgroundImage = UIImage(data: imageData)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -11,14 +11,42 @@ import SwiftUI
 struct EmojiArtDocumentView: View {
     @ObservedObject var document: EmojiArtDocument
     var body: some View {
-        HStack {
-            ForEach(EmojiArtDocument.palette.map { String($0)}, id: \.self ){ emoji in
-                Text(emoji)
-                    .font(Font.system(size: self.defaultEmojiSize))
+        VStack {
+            ScrollView(.horizontal){
+                HStack {
+                    ForEach(EmojiArtDocument.palette.map { String($0)}, id: \.self ){ emoji in
+                        Text(emoji)
+                            .font(Font.system(size: self.defaultEmojiSize))
+                    }
+                }
             }
+            .padding(.horizontal)
+            
+            Color.yellow.overlay(
+                Group {
+                    if self.document.backgroundImage != nil {
+                        Image(uiImage: self.document.backgroundImage!).resizable()
+                    }
+                }
+            )
+                .edgesIgnoringSafeArea([.horizontal,.bottom])
+                .onDrop(of: ["public.image"], isTargeted: nil){ providers, location in
+                    return self.drop(providers: providers)
+                }
         }
-        
+    }
+    
+    private func drop(providers: [NSItemProvider]) -> Bool {
+        let found = providers.loadFirstObject(ofType: URL.self){ url in
+            print("dropped \(url)")
+            self.document.setBackgroundURL(url)
+        }
+        return found
     }
     
     private let defaultEmojiSize: CGFloat = 40
 }
+
+
+
+
