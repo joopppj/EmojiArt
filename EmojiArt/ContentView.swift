ed13e4cc2b6e95b7ca51535b2619978d36d090c8
Kira <jooppp@live.com>
2021-02-17 11:01:08 +0900

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by キラ on 2/17/21.
+//  Copyright © 2021 Kira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
