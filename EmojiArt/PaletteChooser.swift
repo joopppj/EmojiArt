6cbcbc615923e10d5bd481a3d5dd97ab0921bee3
Kira <jooppp@live.com>
2021-02-25 23:18:51 +0900

    Sheet made, use grid to hold emojis to remove and some modifications on grid

--- EmojiArt/PaletteChooser.swift
+++ EmojiArt/PaletteChooser.swift
@@ -12,6 +12,9 @@ struct PaletteChooser: View {
     @Binding var chosenPalette: String 
     
     @ObservedObject var document: EmojiArtDocument
+    
+    @State private var showPaletteEditor = false
+    
     var body: some View {
         HStack{
             Stepper(onIncrement: {
@@ -20,10 +23,73 @@ struct PaletteChooser: View {
                 self.chosenPalette = self.document.palette(before: self.chosenPalette)
             }, label: { Text("")})
             Text(self.document.paletteNames[self.chosenPalette] ?? "")
+            Image(systemName: "keyboard").imageScale(.large)
+                .onTapGesture {
+                    self.showPaletteEditor = true
+            }
+                .sheet(isPresented: $showPaletteEditor){
+                    PaletteEditor(chosenPalette: self.$chosenPalette, isShowing: self.$showPaletteEditor)
+                        .environmentObject(self.document)
+                        .frame(minWidth: 300, minHeight: 500)
+            }
         }
         .fixedSize(horizontal: true, vertical: false)
     }
 }
+
+struct PaletteEditor: View { // popover
+    @EnvironmentObject var document: EmojiArtDocument
+    @Binding var chosenPalette: String
+    @Binding var isShowing: Bool
+    @State private var paletteName: String = ""
+    @State private var emojisToAdd: String = ""
+    var body: some View{
+        VStack(spacing:0){
+            ZStack{
+                Text("Editor").font(.headline).padding()
+                HStack {
+                    Spacer()
+                    Button(action: {self.isShowing = false}, label: { Text("Done")  }).padding()
+                    }
+            }
+                Divider()
+            Form{
+                Section{
+                    TextField("Palette Name", text: $paletteName, onEditingChanged: { began in
+                        if !began{
+                            self.document.renamePalette(self.chosenPalette, to: self.paletteName)
+                        }
+                    })
+                    TextField("Add Emoji", text: $emojisToAdd, onEditingChanged: { began in
+                        if !began{
+                            self.chosenPalette = self.document.addEmoji(self.emojisToAdd, toPalette: self.chosenPalette)
+                            self.emojisToAdd = ""
+                        }
+                    })
+                }
+                Section(header: Text("Remove Emoji")){
+                    
+                    Grid(chosenPalette.map{ String($0) }, id: \.self) { emoji in
+                        Text(emoji).font(Font.system(size: self.fontSize))
+                                .onTapGesture {
+                                    self.chosenPalette = self.document.removeEmoji(emoji, fromPalette: self.chosenPalette)
+                            }
+                    }
+                    .frame(height: self.height)
+                }
+            }
+        }
+        .onAppear{ self.paletteName = self.document.paletteNames[self.chosenPalette] ?? "" }
+    }
+    var height: CGFloat {
+        CGFloat(((chosenPalette.count-1)/6)*70 + 70)
+    }
+    
+    let fontSize: CGFloat = 40
+    
+    
+}
+
 
 struct PaletteChooser_Previews: PreviewProvider {
     static var previews: some View {
